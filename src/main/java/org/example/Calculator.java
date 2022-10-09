6c395f205749e4874e3c3684ba632c86e9f139a0
Noel Peter <noelpeter1111gmail.com>
2022-10-09 12:48:52 +0100

    ''

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,98 +1,84 @@
 package org.example;
 
-// Importing libraries
 import java.util.Scanner;
+import java.util.Stack;
 import java.util.StringTokenizer;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class Calculator
+public class Temp
 {
 
-    public static ArrayList<String> bodmas(String digits)
-    {
-        ArrayList<String> newSum = new ArrayList<>();                    // Creating array list for all digits
-        StringTokenizer token = new StringTokenizer(digits, " ");
+    public static int calculate(String expression) {
+		char[] expressionArray = expression.toCharArray();
+		Stack<Integer> numStack = new Stack<Integer>();
+		Stack<Character> operStack = new Stack<Character>();
+		for (int i = 0; i < expressionArray.length; i++) {
+			boolean invalid;
+			if (expressionArray[i] == ' ') {
+				continue;
+			} else if (expressionArray[i] >= '0' && expressionArray[i] <= '9') {
+				StringBuffer buff_s = new StringBuffer();
+				while (i < expressionArray.length && expressionArray[i] >= '0' && expressionArray[i] <= '9')
+					buff_s.append(expressionArray[i++]);
+				numStack.push(Integer.parseInt(buff_s.toString()));
+			} else if (expressionArray[i] == '(') {
+				operStack.push(expressionArray[i]);
+			} else if (expressionArray[i] == ')') {
+				while (operStack.peek() != '(')
+					numStack.push(applyOperation(operStack.pop(), numStack.pop(), numStack.pop()));
+				operStack.pop();
+			} else if (expressionArray[i] == '+' || expressionArray[i] == '-' || expressionArray[i] == '*'
+					|| expressionArray[i] == '/') {
+				// check if next char is an operator as well
+				int j = i + 1;
+				if (expressionArray[j] == '+' || expressionArray[j] == '-' || expressionArray[j] == '*'
+						|| expressionArray[j] == '/') {
+					invalid = true;
+					break;
+				}
+				while (!operStack.empty() && hasPrecedence(expressionArray[i], operStack.peek()))
+					numStack.push(applyOperation(operStack.pop(), numStack.pop(), numStack.pop()));
+				operStack.push(expressionArray[i]);
+			} else {
+				while (i < expressionArray.length && expressionArray[i] != '0' && expressionArray[i] != '9'
+						&& (expressionArray[i] != '+' || expressionArray[i] != '-' || expressionArray[i] != '*'
+								|| expressionArray[i] != '/'))
+					continue;
+				invalid = true;
+			}
+		}
 
-        Iterator<Object> count = token.asIterator();
-        while (count.hasNext())
-        {
-            newSum.add((String) count.next());
-        }
+		while (!operStack.empty())
+			numStack.push(applyOperation(operStack.pop(), numStack.pop(), numStack.pop()));
+		return numStack.pop();
+	}
 
-        if (newSum.contains("*"))     // Creating a fix for multiplication which will have repeated methods for both
-        // subtraction and addition
-        {
-            do
-            {
-                int addOne = Integer.parseInt(newSum.get(newSum.indexOf("*") + 1));
-                int addTwo = Integer.parseInt(newSum.get(newSum.indexOf("*") - 1));
+	public static boolean hasPrecedence(char oper_1, char oper_2) {
+		if (oper_2 == '(' || oper_2 == ')')
+			return false;
+		if (oper_1 == '*' && (oper_2 == '+' || oper_2 == '-'))
+			return false;
+		else
+			return true;
+	}
 
-                int solution = addTwo * addOne;
+	public static int applyOperation(char oper, int var2, int var1) {
+		switch (oper) {
+		case '+':
+			return var1 + var2;
+		case '-':
+			return var1 - var2;
+		case '*':
+			return var1 * var2;
+		}
+		return 0;
+	}
 
-                newSum.set(newSum.indexOf("*") - 1, Integer.toString(solution));
-                newSum.remove(newSum.indexOf("*") + 1);
-                newSum.remove(newSum.indexOf("*"));
-            } while (newSum.contains("*"));
-        }
-
-        if (newSum.contains("-"))
-        {
-            do
-            {
-                int addOne = Integer.parseInt(newSum.get(newSum.indexOf("-") + 1));
-                int addTwo = Integer.parseInt(newSum.get(newSum.indexOf("-") - 1));
-
-                int solution = addTwo - addOne;
-
-                newSum.set(newSum.indexOf("-") - 1, Integer.toString(solution));
-                newSum.remove(newSum.indexOf("-") + 1);
-                newSum.remove(newSum.indexOf("-"));
-            } while (newSum.contains("-"));
-        }
-
-        if (newSum.contains("+"))
-        {
-            do
-            {
-                int addOne = Integer.parseInt(newSum.get(newSum.indexOf("+") + 1));
-                int addTwo = Integer.parseInt(newSum.get(newSum.indexOf("+") - 1));
-
-                int solution = addTwo + addOne;
-
-                newSum.set(newSum.indexOf("+") - 1, Integer.toString(solution));
-                newSum.remove(newSum.indexOf("+") + 1);
-                newSum.remove(newSum.indexOf("+"));
-            } while (newSum.contains("+"));
-        }
-    /*  else if(newSum.equals("++"))
-        {
-                System.out.println("Invalid pls type in your equation again:");
-        } */
-
-        return newSum;
-    }
-
-    public static void main(String[] args)
-    {
-        System.out.println("Pls type in your equation:");
+	public static void main(String[] args) {
+		System.out.print("Please enter a valid expression to calculate : ");
         Scanner input = new Scanner(System.in);
-        String num = input.nextLine();
-
-        for(String digit : bodmas(num))   // Equation will be solved for the user
-        {
-            System.out.println(digit);
-
-         /*  if(digit.equalsIgnoreCase( "++")|| digit.equalsIgnoreCase("--")|| digit.equalsIgnoreCase("**"))
-            {
-                System.out.println("Invalid pls type in your equation again:");
-            }
-            else
-            {
-                break;
-            }*/
-        }
-    }
-
-    // Test 12435 + 34569 - 12345 * 10 + 50
+        String exp = input.nextLine();
+		System.out.println(calculate(exp));
+	}
 }
